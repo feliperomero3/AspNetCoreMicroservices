d0ac55916a0be19ef2ee0c26c00e6b2b72929e82
Felipe Romero <feliperomero3@outlook.com>
2022-01-20 22:06:20 -0600

    Add seed test data.

--- src/ShoppingCart/ShoppingCartStore.cs
+++ src/ShoppingCart/ShoppingCartStore.cs
@@ -6,8 +6,30 @@ namespace ShoppingCart
     {
         private static readonly Dictionary<int, ShoppingCart> Database = new();
 
+        public ShoppingCartStore()
+        {
+            Seed();
+        }
+
         public ShoppingCart Get(int userId) => Database.ContainsKey(userId) ? Database[userId] : new ShoppingCart(userId);
 
         public void Save(ShoppingCart shoppingCart) => Database[shoppingCart.UserId] = shoppingCart;
+
+        public static void Seed()
+        {
+            if (Database.Count > 0)
+            {
+                return;
+            }
+
+            var item1 = new ShoppingCartItem(1, "Basic t-shirt", "a quiet t-shirt", new Money("eur", 40));
+            var item2 = new ShoppingCartItem(2, "Fancy shirt", "a loud t-shirt", new Money("eur", 50));
+
+            var cart = new ShoppingCart(42);
+
+            cart.AddItems(new[] { item1, item2 });
+
+            Database.Add(42, cart);
+        }
     }
 }
