93ffa73b8679c5eb71d35e8f9c3955bca0151f35
Felipe Romero <feliperomero3@outlook.com>
2022-02-01 01:04:28 -0600

    Add Microsoft Identity Platform as a service dependency.

--- src/ShoppingCart/Startup.cs
+++ src/ShoppingCart/Startup.cs
@@ -1,20 +1,43 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Web;
+using Microsoft.IdentityModel.Logging;
 
 namespace ShoppingCart
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMicrosoftIdentityWebApiAuthentication(Configuration);
+
             services.AddControllers();
+
+            services.AddAuthorization(options =>
+            {
+                options.FallbackPolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+            });
+
             services.AddScoped<ShoppingCartStore>();
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            IdentityModelEventSource.ShowPII = true;
             app.UseHttpsRedirection();
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
