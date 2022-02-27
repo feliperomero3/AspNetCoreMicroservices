f4957fc8309df65d8fdd7b09c196a064e2d8cd2c
Felipe Romero <feliperomero3@outlook.com>
2022-02-26 23:58:39 -0600

    Adding a failure-handling policy.

--- src/ShoppingCart/Startup.cs
+++ src/ShoppingCart/Startup.cs
@@ -1,9 +1,11 @@
+using System;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Identity.Web;
 using Microsoft.IdentityModel.Logging;
+using Polly;
 
 namespace ShoppingCart
 {
@@ -29,7 +31,10 @@ namespace ShoppingCart
 
             services.AddScoped<ShoppingCartStore>();
             services.AddScoped<ProductCatalogClient>();
-            services.AddHttpClient();
+            services.AddHttpClient<ProductCatalogClient>()
+                .AddTransientHttpErrorPolicy(policy =>
+                    policy.WaitAndRetryAsync(3, attempt => TimeSpan.FromMilliseconds(100 * Math.Pow(2, attempt)))
+                );
         }
 
         public void Configure(IApplicationBuilder app)
